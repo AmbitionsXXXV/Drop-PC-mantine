8e188899add12d3fe2b1da978e67f37bfb6beeea
etctera <yilonglei790@gmail.com>
2024-01-09 09:45:39 +0800

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import '@mantine/core/styles.css'
 import { MantineProvider } from '@mantine/core'
-import { Router } from './Router'
+import '@mantine/core/styles.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import MainLayout from './layout/MainLayout'
+import { ROUTE_COMPONENT } from './routes'
+import { routes } from './routes/menu'
 import { theme } from './theme'
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Router />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<div>login</div>} />
+
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((item) => {
+              const Component = ROUTE_COMPONENT[item.key]
+              return <Route path={item.path} key={item.key} element={<Component />} />
+            })}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   )
 }
