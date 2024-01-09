ca0d6d306e7b5a25f62ae6853361724b28cd6ae8
etctera <yilonglei790@gmail.com>
2024-01-09 15:15:57 +0800

    feat: main layout

---
+++ src/components/OrgSelect/index.tsx
@@ -0,0 +1,72 @@
+import { useGoTo } from '@/hooks'
+import { useUserContext } from '@/hooks/userHooks'
+import { ROUTE_KEY } from '@/routes/menu'
+import { useOrganizations } from '@/service/org'
+import { currentOrg } from '@/utils'
+import { LOCAL_CURRENT_ORG } from '@/utils/constants'
+import { ComboboxItem, Select, Space } from '@mantine/core'
+import _ from 'lodash'
+import { useEffect, useState } from 'react'
+
+/**
+ *  门店选择器
+ */
+const OrgSelect = () => {
+  const [options, setOptions] = useState<
+    | Array<{
+        label: string
+        value: string
+      }>
+    | undefined
+  >([])
+  const { data, refetch } = useOrganizations(1, 10, true)
+  const { go } = useGoTo()
+  const { setStore } = useUserContext()
+
+  const onSearchHandler = _.debounce((name: string) => {
+    refetch({
+      name
+    })
+  }, 500)
+
+  const onChangeHandler = (_value: string | null, option: ComboboxItem) => {
+    setStore({
+      currentOrg: option.value
+    })
+    localStorage.setItem(LOCAL_CURRENT_ORG, JSON.stringify(option))
+  }
+
+  useEffect(() => {
+    if (currentOrg()?.value) {
+      setStore({
+        currentOrg: currentOrg().value
+      })
+    } else {
+      go(ROUTE_KEY.NO_ORG)
+    }
+  }, [])
+
+  useEffect(() => {
+    const _options = data?.map((item) => ({ label: item.name, value: item.id }))
+
+    setOptions(_options)
+  }, [data])
+
+  return (
+    <Space className="flex items-center">
+      选择门店：
+      <Select
+        searchable
+        data={options}
+        style={{ width: 200 }}
+        placeholder="请选择门店"
+        defaultValue={currentOrg()}
+        // @ts-ignore
+        onChange={onChangeHandler}
+        onSearchChange={onSearchHandler}
+      />
+    </Space>
+  )
+}
+
+export default OrgSelect
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,122 @@
-import { Title } from '@mantine/core'
+import NavLinks from '@/components/NavLinks'
+import OrgSelect from '@/components/OrgSelect'
+import { useGoTo, useIsOrgRoute } from '@/hooks'
+import { useUserContext } from '@/hooks/userHooks'
+import Home from '@/pages/Home/Home.page'
+import { ROUTE_KEY } from '@/routes/menu'
+import { AUTH_TOKEN } from '@/utils/constants'
+import { LogoutOutlined, ShopOutlined } from '@ant-design/icons'
+import {
+  AppShell,
+  Burger,
+  Divider,
+  Group,
+  ScrollArea,
+  Space,
+  Text,
+  Tooltip
+} from '@mantine/core'
+import { useToggle } from '@mantine/hooks'
 import type { FC } from 'react'
-import { Outlet } from 'react-router-dom'
+import { useNavigate, useOutlet } from 'react-router-dom'
+import classes from './main-layout.module.css'
+
+const { Header, Main, Navbar, Section } = AppShell
 
 const MainLayout: FC = () => {
+  const [collapsed, toggleCollapsed] = useToggle([true, false])
+  const outlet = useOutlet()
+  const { store } = useUserContext()
+  const isOrg = useIsOrgRoute()
+  const { go } = useGoTo()
+  const nav = useNavigate()
+
+  const logoutHandler = () => {
+    sessionStorage.setItem(AUTH_TOKEN, '')
+    localStorage.setItem(AUTH_TOKEN, '')
+
+    nav('/login')
+  }
+
+  const goToOrg = () => {
+    go(ROUTE_KEY.ORG)
+  }
+
   return (
     <>
-      <Title className="text-[color:var(--mantine-color-black)]">Main Layout</Title>
+      <AppShell
+        padding="md"
+        header={{ height: 60 }}
+        className={classes.appShell}
+        navbar={{
+          width: 200,
+          breakpoint: 'sm',
+          collapsed: { desktop: !collapsed }
+        }}
+        aside={{
+          breakpoint: 'sm',
+          width: { sm: 200, lg: 300 },
+          collapsed: { desktop: false }
+        }}
+      >
+        <Header data-tauri-drag-region p="md" className={classes.header}>
+          <Group h="100%" justify="space-between" className="w-full">
+            <Group>
+              <Burger
+                size="sm"
+                hiddenFrom="sm"
+                opened={collapsed}
+                onClick={() => toggleCollapsed()}
+              />
 
-      <Outlet />
+              <Burger
+                size="sm"
+                visibleFrom="sm"
+                opened={collapsed}
+                onClick={() => toggleCollapsed()}
+              />
+
+              <Text>Header Title</Text>
+            </Group>
+
+            <Group>
+              {!isOrg && <OrgSelect />}
+
+              <Tooltip label="门店管理">
+                <ShopOutlined onClick={goToOrg} />
+              </Tooltip>
+            </Group>
+          </Group>
+        </Header>
+
+        <Navbar p="xs" className="overflow-y-auto">
+          <Section grow component={ScrollArea}>
+            <NavLinks
+              views={[{ component: Home, path: '/home', name: 'Home' }]}
+              toggleCollapsed={toggleCollapsed}
+            />
+          </Section>
+
+          <Section className="pb-1">
+            <Divider />
+          </Section>
+
+          <Section>
+            <Space
+              h={28}
+              onClick={logoutHandler}
+              className="w-full flex gap-3 justify-center items-center cursor-pointer hover:bg-slate-100"
+            >
+              <LogoutOutlined size={14} />
+              退出
+            </Space>
+          </Section>
+        </Navbar>
+
+        <Main className={classes.appMain}>
+          <div key={store.currentOrg}>{outlet}</div>
+        </Main>
+      </AppShell>
     </>
   )
 }
