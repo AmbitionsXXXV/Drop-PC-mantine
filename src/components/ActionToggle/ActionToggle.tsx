a17e1a53a5baed76a97707cc69ba6546d1202168
etctera <yilonglei790@gmail.com>
2024-01-11 11:32:12 +0800

    feat: theme toggle button component

---
+++ src/components/ActionToggle/ActionToggle.tsx
@@ -0,0 +1,30 @@
+import {
+  ActionIcon,
+  Group,
+  useComputedColorScheme,
+  useMantineColorScheme
+} from '@mantine/core'
+import { IconMoon, IconSun } from '@tabler/icons-react'
+import cx from 'clsx'
+import classes from './ActionToggle.module.css'
+
+export function ActionToggle() {
+  const { setColorScheme } = useMantineColorScheme()
+  const computedColorScheme = useComputedColorScheme('light', {
+    getInitialValueInEffect: true
+  })
+
+  return (
+    <Group justify="center">
+      <ActionIcon
+        size="xl"
+        variant="default"
+        aria-label="Toggle color scheme"
+        onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+      >
+        <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
+        <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
+      </ActionIcon>
+    </Group>
+  )
+}
