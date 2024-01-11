e9ba44b72a24c1791c8777cc94193a7e1ef04a55
etctera <yilonglei790@gmail.com>
2024-01-11 11:32:41 +0800

    feat: main layout

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,29 +1,25 @@
-import NavLinks from '@/components/NavLinks'
+import { NavbarNested } from '@/components/NavbarNested/NavbarNested'
 import { useUserContext } from '@/hooks/userHooks'
-import { routes } from '@/routes/menu'
-import { AUTH_TOKEN } from '@/utils/constants'
-import { LogoutOutlined } from '@ant-design/icons'
-import { AppShell, Divider, ScrollArea, Space } from '@mantine/core'
+import { AppShell } from '@mantine/core'
 import { useToggle } from '@mantine/hooks'
 import type { FC } from 'react'
-import { useNavigate, useOutlet } from 'react-router-dom'
+import { useOutlet } from 'react-router-dom'
 import MainHeader from '../MainHeader/MainHeader'
 import classes from './main-layout.module.css'
 
-const { Main, Navbar, Section } = AppShell
+const { Main } = AppShell
 
 const MainLayout: FC = () => {
   const [collapsed, toggleCollapsed] = useToggle([true, false])
   const outlet = useOutlet()
   const { store } = useUserContext()
-  const nav = useNavigate()
 
-  const logoutHandler = () => {
-    sessionStorage.setItem(AUTH_TOKEN, '')
-    localStorage.setItem(AUTH_TOKEN, '')
+  // const logoutHandler = () => {
+  //   sessionStorage.setItem(AUTH_TOKEN, '')
+  //   localStorage.setItem(AUTH_TOKEN, '')
 
-    nav('/login')
-  }
+  //   nav('/login')
+  // }
 
   return (
     <>
@@ -44,26 +40,7 @@ const MainLayout: FC = () => {
       >
         <MainHeader collapsed={collapsed} toggleCollapsed={toggleCollapsed} />
 
-        <Navbar p="xs">
-          <Section grow component={ScrollArea}>
-            <NavLinks views={routes} toggleCollapsed={toggleCollapsed} />
-          </Section>
-
-          <Section className="pb-1">
-            <Divider />
-          </Section>
-
-          <Section>
-            <Space
-              h={28}
-              onClick={logoutHandler}
-              className="w-full flex gap-3 justify-center items-center cursor-pointer hover:bg-slate-100"
-            >
-              <LogoutOutlined size={14} />
-              退出
-            </Space>
-          </Section>
-        </Navbar>
+        <NavbarNested />
 
         <Main className={classes.appMain}>
           <div key={store.currentOrg}>{outlet}</div>
