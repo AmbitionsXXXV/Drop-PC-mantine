e4d1f29641daaa5af89cff5f097dfb6a6475446f
etctera <yilonglei790@gmail.com>
2024-01-11 23:12:47 +0800

    feat: add mantine date

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { MantineProvider } from '@mantine/core'
+import { DatesProvider } from '@mantine/dates'
+import '@mantine/dates/styles.css'
 import 'dayjs/locale/zh-cn'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -7,6 +9,8 @@ import { theme } from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <MantineProvider theme={theme}>
-    <App />
+    <DatesProvider settings={{ locale: 'zh' }}>
+      <App />
+    </DatesProvider>
   </MantineProvider>
 )
