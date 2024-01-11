be7384443b84f71931330095f9c9241cc59e6133
etctera <yilonglei790@gmail.com>
2024-01-11 23:40:09 +0800

    feat: course demo

--- src/pages/Course/Course.page.tsx
+++ src/pages/Course/Course.page.tsx
@@ -1,9 +1,41 @@
-import type { FC } from 'react'
+import { useCourses } from '@/service/course'
+import { Table } from '@mantine/core'
+import { useState, type FC } from 'react'
+import { tableData } from './constants'
 
 const Course: FC = () => {
+  const [curId, setCurId] = useState('')
+  const { refetch } = useCourses()
+  const [showInfo, setShowInfo] = useState(false)
+  const [showOrderTime, setShowOrderTime] = useState(false)
+  const [showCard, setShowCard] = useState(false)
+
+  const onClickAddHandler = (id?: string) => {
+    if (id) {
+      setCurId(id)
+    } else {
+      setCurId('')
+    }
+    setShowInfo(true)
+  }
+
+  const onOrderTimeHandler = (id: string) => {
+    setCurId(id)
+    setShowOrderTime(true)
+  }
+
+  const onCardHandler = (id: string) => {
+    setCurId(id)
+    setShowCard(true)
+  }
+
   return (
     <>
       <div>Course</div>
+
+      <Table.ScrollContainer minWidth={800}>
+        <Table striped verticalSpacing="xs" data={tableData} />
+      </Table.ScrollContainer>
     </>
   )
 }
---
+++ src/pages/Course/constants.tsx
@@ -0,0 +1,5 @@
+import { TableData } from '@mantine/core'
+
+export const tableData: TableData = {
+  head: ['课程标题', '限制人数', '持续时长', '操作']
+}
